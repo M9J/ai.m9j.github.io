6e42ecf6e12c03dff9cef716ea0589e6fb246655
Martian Jeirgif <mgj.exe@gmail.com>
2025-03-17 21:11:35 +0530

    dir cleanup

--- dataset/intent-recognition/data.js
+++
@@ -1,6 +0,0 @@
-const data = {
-    greeting: ["hello", "hi", "hey"],
-    goodbye: ["bye", "goodbye", "see you later"],
-    thanks: ["thank you", "thanks", "much appreciated"],
-};
-export default data;
